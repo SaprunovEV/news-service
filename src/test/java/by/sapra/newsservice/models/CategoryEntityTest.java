4a6a6a7df86ce0a5965e81f4748a5d9bdd2a31c9
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-21 21:33:32 +0300

    Создать основную модель приложения. #9: С помощью схемы определить сущность для категории CategoryEntity:
    Подготовлена основа для тестов с ошибками в базе.

--- src/test/java/by/sapra/newsservice/models/CategoryEntityTest.java
+++ src/test/java/by/sapra/newsservice/models/CategoryEntityTest.java
@@ -1,12 +1,13 @@
 package by.sapra.newsservice.models;
 
-import by.sapra.newsservice.config.AbstractDataTest;
+import by.sapra.newsservice.config.AbstractMigrationTest;
 import org.junit.jupiter.api.Test;
 
 import static by.sapra.newsservice.testUtils.CategoryTestDataBuilder.aCategory;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class CategoryEntityTest extends AbstractDataTest {
+class CategoryEntityTest extends AbstractMigrationTest {
     @Test
     void shouldSaveTheEntityAndTakeId() throws Exception {
         CategoryEntity savedEntity = getTestDbFacade().save(aCategory());
