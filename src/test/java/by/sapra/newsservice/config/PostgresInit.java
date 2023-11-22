91f9ee01cf4ad2b11df44f0f6f1662a49963356f
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-22 17:27:05 +0300

    Создать основную модель приложения. #9: С помощью схемы определить сущность для комментария (CommentaryEntity). #13:
    добавлена интеграция с docker-compose.yml, схема для базы данных, и параметры для подключения базы данных. TestContainer настроен на нужную схему.

--- src/test/java/by/sapra/newsservice/config/PostgresInit.java
+++ src/test/java/by/sapra/newsservice/config/PostgresInit.java
@@ -8,7 +8,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 public class PostgresInit {
     public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:14.5")
             .withDatabaseName("news-service")
-            .withReuse(true);
+            .withReuse(true)
+            .withInitScript("static/schema.sql");
 
     public static class PostgresApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         @Override
