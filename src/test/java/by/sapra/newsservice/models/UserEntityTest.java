c34b4a90a6ae529a7bf64f43b80f63a1191de816
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-22 11:03:00 +0300

    Создать основную модель приложения. #9: С помощью схемы определить сущность для пользователя userEntity:
    созданы тесты для проверки ограничений для имени сущности пользователя.

--- src/test/java/by/sapra/newsservice/models/UserEntityTest.java
+++ src/test/java/by/sapra/newsservice/models/UserEntityTest.java
@@ -2,8 +2,12 @@ package by.sapra.newsservice.models;
 
 import by.sapra.newsservice.config.AbstractMigrationTest;
 import by.sapra.newsservice.testUtils.UserTestDataBuilder;
+import net.bytebuddy.utility.RandomString;
+import org.hibernate.exception.ConstraintViolationException;
+import org.hibernate.exception.DataException;
 import org.junit.jupiter.api.Test;
 
+import static by.sapra.newsservice.testUtils.UserTestDataBuilder.aUser;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserEntityTest extends AbstractMigrationTest {
@@ -17,5 +21,23 @@ class UserEntityTest extends AbstractMigrationTest {
 
             assertEquals(savedEntity.getName(), getTestDbFacade().find(savedEntity.getId(), UserEntity.class).getName());
         });
+    }
+
+    @Test
+    void shouldThrowExceptionIfNameIsNull() throws Exception {
+        assertThrows(ConstraintViolationException.class, this::executeNullName);
+    }
+
+    @Test
+    void shouldThrowExceptionIfLengthOfNameMoreThenPossible() throws Exception {
+        assertThrows(DataException.class, this::executeLongName);
+    }
+
+    private void executeLongName() {
+        getTestTransactionExecuter().execute(() -> getTestDbFacade().save(aUser().withName(RandomString.make(51))));
+    }
+
+    private void executeNullName() {
+        getTestTransactionExecuter().execute(() -> getTestDbFacade().save(aUser().withName(null)));
     }
 }
