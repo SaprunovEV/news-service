911a7c9dfbd95265f2bac3e7156f1d5a79425ad8
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-23 14:40:35 +0300

    Создать API для новостей. #19: создать и протестировать контроллер #20:
    удалён неиспользуемый импорт.

--- src/main/java/by/sapra/newsservice/services/NewsService.java
+++ src/main/java/by/sapra/newsservice/services/NewsService.java
@@ -2,7 +2,6 @@ package by.sapra.newsservice.services;
 
 import by.sapra.newsservice.services.models.News;
 import by.sapra.newsservice.services.models.filters.NewsFilter;
-import by.sapra.newsservice.web.v1.models.NewsListResponse;
 
 import java.util.List;
 
