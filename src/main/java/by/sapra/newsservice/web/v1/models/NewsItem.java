d3aa5f3bb64d4f81d356228a4c9c6b69f0c8e88e
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-23 14:07:22 +0300

    Создать API для новостей. #19: создать и протестировать контроллер #20:
    Закончены тесты поиска всех новостей. Было заменено поле abstract на newAbstract по причине экономии времени на решение проблем с запуском тестов.

--- src/main/java/by/sapra/newsservice/web/v1/models/NewsItem.java
+++ src/main/java/by/sapra/newsservice/web/v1/models/NewsItem.java
@@ -1,6 +1,5 @@
 package by.sapra.newsservice.web.v1.models;
 
-import jakarta.persistence.JoinColumn;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +8,7 @@ import lombok.Data;
 public class NewsItem {
     private Long id;
     private String title;
-    @JoinColumn(name = "abstract")
     private String newsAbstract;
     private String body;
-    private Long commentCount;
+    private Long commentsCount;
 }
