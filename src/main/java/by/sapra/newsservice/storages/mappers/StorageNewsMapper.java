671d5d18368387544d843485ff7ab6dced176618
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-24 16:59:44 +0300

    Создать API для новостей. #19: создать и протестировать контроллер #20:
    реализован преобразователь для хранения новостей.

--- src/main/java/by/sapra/newsservice/storages/mappers/StorageNewsMapper.java
+++ src/main/java/by/sapra/newsservice/storages/mappers/StorageNewsMapper.java
@@ -2,10 +2,24 @@ package by.sapra.newsservice.storages.mappers;
 
 import by.sapra.newsservice.models.NewsEntity;
 import by.sapra.newsservice.storages.models.NewsListModel;
+import by.sapra.newsservice.storages.models.NewsModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = IGNORE, uses = {StorageCommentMapper.class})
 public interface StorageNewsMapper {
-    NewsListModel entitiesListToNewsListModel(List<NewsEntity> content, HashMap<Long, Long> countsMap);
+    default NewsListModel entitiesListToNewsListModel(List<NewsEntity> content, Map<Long, Long> countsMap) {
+        return NewsListModel.builder()
+                .news(content.stream().map(entity -> entityToModel(entity, countsMap.get(entity.getId()))).toList())
+                .count(countsMap.values().stream().reduce(0L, Long::sum))
+                .build();
+    }
+
+    @Mapping(source = "count", target = "commentSize")
+    NewsModel entityToModel(NewsEntity entity, Long count);
 }
