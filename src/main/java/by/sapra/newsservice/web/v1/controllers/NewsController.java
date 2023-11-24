25ca302b727859267bb9375bd93be3cba2f36d1c
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-24 17:10:49 +0300

    Создать API для новостей. #19:
    Добавлена документация на метод контроллера.

--- src/main/java/by/sapra/newsservice/web/v1/controllers/NewsController.java
+++ src/main/java/by/sapra/newsservice/web/v1/controllers/NewsController.java
@@ -4,6 +4,11 @@ import by.sapra.newsservice.services.NewsService;
 import by.sapra.newsservice.services.models.filters.NewsFilter;
 import by.sapra.newsservice.web.v1.mappers.NewsMapper;
 import by.sapra.newsservice.web.v1.models.NewsListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
+@Tag(name = "news_v1", description = "News API version V1")
 public class NewsController {
     private final NewsService service;
     private final NewsMapper mapper;
     @GetMapping
+    @Operation(
+            summary = "Get all news.",
+            description = "Get all news. Return list of news without comments.",
+            tags = {"news"}
+    )
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = NewsListResponse.class))
+    )
     public ResponseEntity<NewsListResponse> findAll(NewsFilter filter) {
         return ResponseEntity.ok(
                 mapper.newsListToNewsListResponse(service.findAll(filter))
