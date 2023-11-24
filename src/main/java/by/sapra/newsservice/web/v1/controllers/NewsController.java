5a888f1320fb5e9b5c96b23b91fcec6d353f63e9
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-24 18:35:09 +0300

    Создать API для новостей. #19:
    отредактирована документация для метода.

--- src/main/java/by/sapra/newsservice/web/v1/controllers/NewsController.java
+++ src/main/java/by/sapra/newsservice/web/v1/controllers/NewsController.java
@@ -4,8 +4,11 @@ import by.sapra.newsservice.services.NewsService;
 import by.sapra.newsservice.services.models.filters.NewsFilter;
 import by.sapra.newsservice.web.v1.mappers.NewsMapper;
 import by.sapra.newsservice.web.v1.models.NewsListResponse;
+import by.sapra.newsservice.web.v1.models.PaginationErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,11 +30,29 @@ public class NewsController {
     @Operation(
             summary = "Get all news.",
             description = "Get all news. Return list of news without comments.",
+            method = "GET",
+            parameters = {
+                    @Parameter(
+                            name = "pageNumber",
+                            schema = @Schema(implementation = Long.class),
+                            examples = {@ExampleObject(value = "0")}),
+                    @Parameter(
+                            name = "pageSize",
+                            schema = @Schema(implementation = Long.class),
+                            examples = {@ExampleObject(value = "3")})
+            },
             tags = {"news"}
     )
     @ApiResponse(
             responseCode = "200",
             content = @Content(schema = @Schema(implementation = NewsListResponse.class))
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Error of pagination.",
+            content = @Content(
+                    schema = @Schema(implementation = PaginationErrorResponse.class),
+                    examples = {@ExampleObject(value = "{\n\"message\": \"Номер страницы должен быть заполнен!\"\n}")})
     )
     public ResponseEntity<NewsListResponse> findAll(@Valid NewsFilter filter) {
         return ResponseEntity.ok(
