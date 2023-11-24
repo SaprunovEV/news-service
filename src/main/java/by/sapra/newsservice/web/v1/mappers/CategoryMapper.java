ca400023b2806fceacd61260fab6fb73938234af
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-24 23:46:54 +0300

    Создать API для получения всех категорий. #26
    Разработан преобразователь для API категорий.

--- src/main/java/by/sapra/newsservice/web/v1/mappers/CategoryMapper.java
+++ src/main/java/by/sapra/newsservice/web/v1/mappers/CategoryMapper.java
@@ -1,10 +1,21 @@
 package by.sapra.newsservice.web.v1.mappers;
 
 import by.sapra.newsservice.services.models.Category;
+import by.sapra.newsservice.web.v1.models.CategoryItem;
 import by.sapra.newsservice.web.v1.models.CategoryListResponse;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = IGNORE, uses = {NewsMapper.class})
 public interface CategoryMapper {
-    CategoryListResponse categoryItemListToCategoryListResponse(List<Category> list);
+    default CategoryListResponse categoryItemListToCategoryListResponse(List<Category> list) {
+        return CategoryListResponse.builder()
+                .categories(list.stream().map(this::categoryToCategoryItem).toList())
+                .build();
+    }
+
+    CategoryItem categoryToCategoryItem(Category category);
 }
