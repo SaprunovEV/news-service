4795f9df9a8ac1eb98014e5dae356cd1675d5cff
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-24 15:57:34 +0300

    Создать API для новостей. #19: создать и протестировать контроллер #20:
    Настроен тест для сервисного преобразования.

---
+++ src/test/java/by/sapra/newsservice/services/mappers/NewsModelMapperTest.java
@@ -0,0 +1,17 @@
+package by.sapra.newsservice.services.mappers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = NewsModelMapperConf.class)
+class NewsModelMapperTest {
+    @Autowired
+    NewsModelMapper mapper;
+    
+}
