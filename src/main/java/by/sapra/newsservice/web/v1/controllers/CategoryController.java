44727c4e2066d675831bf67ff80f7f9b1b4de021
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-27 17:24:15 +0300

    Создать API для получения всех категорий. #26
    добавлена документация для контроллера категорий.

--- src/main/java/by/sapra/newsservice/web/v1/controllers/CategoryController.java
+++ src/main/java/by/sapra/newsservice/web/v1/controllers/CategoryController.java
@@ -3,6 +3,14 @@ package by.sapra.newsservice.web.v1.controllers;
 import by.sapra.newsservice.services.CategoryService;
 import by.sapra.newsservice.services.models.CategoryFilter;
 import by.sapra.newsservice.web.v1.mappers.CategoryMapper;
+import by.sapra.newsservice.web.v1.models.CategoryListResponse;
+import by.sapra.newsservice.web.v1.models.PaginationErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +27,33 @@ public class CategoryController {
     private final CategoryService service;
     private final CategoryMapper mapper;
     @GetMapping
+    @Operation(
+            summary = "Get all categories.",
+            description = "Get all categories. Return list of categories without news.",
+            method = "GET",
+            parameters = {
+                    @Parameter(
+                            name = "pageNumber",
+                            schema = @Schema(implementation = Long.class),
+                            examples = {@ExampleObject(value = "0")}),
+                    @Parameter(
+                            name = "pageSize",
+                            schema = @Schema(implementation = Long.class),
+                            examples = {@ExampleObject(value = "3")})
+            },
+            tags = {"category", "V1"}
+    )
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = CategoryListResponse.class))
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Error of pagination.",
+            content = @Content(
+                    schema = @Schema(implementation = PaginationErrorResponse.class),
+                    examples = {@ExampleObject(value = "{\n\"message\": \"Номер страницы должен быть заполнен!\"\n}")})
+    )
     public ResponseEntity<?> handleFindAll(@Valid CategoryFilter filter) {
         return ResponseEntity.ok(mapper.categoryItemListToCategoryListResponse(service.findAll(filter)));
     }
