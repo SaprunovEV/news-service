b6f4589a365d7af1817bbe2d81c5a68d83ebbdd4
Евгений Сапрунов <sapra10062018@gmail.com>
2023-12-13 14:43:00 +0300

    Разработать метод для сохранения категории без новостей. #37
    завершен слой хранилища данных для обновления категории.

--- src/main/java/by/sapra/newsservice/storages/impl/DatabaseCategoryStorage.java
+++ src/main/java/by/sapra/newsservice/storages/impl/DatabaseCategoryStorage.java
@@ -47,7 +47,17 @@ public class DatabaseCategoryStorage implements CategoryStorage {
 
     @Override
     public Optional<FullCategoryModel> updateCategory(FullCategoryModel model2update) {
-        return null;
+        if (model2update.getId() <= 0 ) return Optional.empty();
+
+        Optional<CategoryEntity> optional = repository.findById(model2update.getId());
+
+        if (optional.isPresent()) {
+            CategoryEntity entity = optional.get();
+            entity.setName(model2update.getName());
+            return Optional.of(mapper.entityToFullCategory(entity));
+        }
+
+        return Optional.empty();
     }
 
     private CategoryListModel mapListModel(Page<CategoryEntity> page) {
