95cffad9d442b08527b143f762ddfe8d082312db
Евгений Сапрунов <sapra10062018@gmail.com>
2023-12-13 11:04:25 +0300

    Разработать метод для сохранения категории без новостей. #37
    создана документация для метода обновления категории.

---
+++ src/main/java/by/sapra/newsservice/web/v1/annotations/UpdateCategoryDock.java
@@ -0,0 +1,40 @@
+package by.sapra.newsservice.web.v1.annotations;
+
+import by.sapra.newsservice.models.errors.CategoryError;
+import by.sapra.newsservice.web.v1.models.CategoryResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(
+        summary = "Updated category by id.",
+        description = "Updated category by id. Return updated category",
+        method = "PUT",
+        tags = {"category", "V1"}
+)
+@ApiResponse(
+        responseCode = "200",
+        content = @Content(schema = @Schema(implementation = CategoryResponse.class))
+)
+@ApiResponse(
+        responseCode = "400",
+        description = "Category already exist.",
+        content = @Content(
+                schema = @Schema(implementation = CategoryError.class),
+                examples = {@ExampleObject(value = "{\n\"message\": \"Категория с именем бизнес уже существует!\"\n}")})
+)
+@ApiResponse(
+        responseCode = "406",
+        description = "Category validation error.",
+        content = @Content(
+                schema = @Schema(implementation = CategoryError.class),
+                examples = {@ExampleObject(value = "{\n\"message\": \"Имя категории должно быть между 5 и 50 символами\"\n}")})
+)
+public @interface UpdateCategoryDock {
+}
