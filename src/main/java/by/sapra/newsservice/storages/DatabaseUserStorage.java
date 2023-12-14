3f25120825ffa56409e04b914097db9338f763bf
Евгений Сапрунов <sapra10062018@gmail.com>
2023-12-14 14:21:41 +0300

    API для поиска всех пользователей; #44
    выполнен слой хранилища для поиска всех пользователей.

---
+++ src/main/java/by/sapra/newsservice/storages/DatabaseUserStorage.java
@@ -0,0 +1,24 @@
+package by.sapra.newsservice.storages;
+
+import by.sapra.newsservice.models.UserEntity;
+import by.sapra.newsservice.services.models.filters.UserFilter;
+import by.sapra.newsservice.storages.mappers.StorageUserMapper;
+import by.sapra.newsservice.storages.models.StorageUserList;
+import by.sapra.newsservice.storages.reposytory.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseUserStorage implements UserStorage {
+    private final UserRepository repository;
+    private final StorageUserMapper mapper;
+
+    @Override
+    public StorageUserList findAll(UserFilter filter) {
+        Page<UserEntity> pageUser = repository.findAll(PageRequest.of(filter.getPageNumber(), filter.getPageSize()));
+        return mapper.entityListToStorageUserList(pageUser.getContent());
+    }
+}
