a6e82aa691c08903c1b94b51511ec76877ea46ac
Евгений Сапрунов <sapra10062018@gmail.com>
2023-12-15 17:11:46 +0300

    API для создания пользователя; #46
    Исправление: внесённые изменения исключают множественный вызов преобразователя. (Не знаю как в lombock исключить поле из builder)

--- src/main/java/by/sapra/newsservice/services/models/UserApplicationModel.java
+++ src/main/java/by/sapra/newsservice/services/models/UserApplicationModel.java
@@ -2,21 +2,36 @@ package by.sapra.newsservice.services.models;
 
 import by.sapra.newsservice.models.errors.UserError;
 import by.sapra.newsservice.storages.models.StorageUserItem;
-import lombok.Builder;
 
 import java.util.Optional;
 import java.util.function.Function;
 
-@Builder
 public class UserApplicationModel implements ApplicationModel<UserItemModel, UserError> {
 
-    private Optional<StorageUserItem> model;
-    private Function<StorageUserItem, UserItemModel> mapper;
-    private String message;
+    private final Optional<StorageUserItem> model;
+    private final Function<StorageUserItem, UserItemModel> mapper;
+    private final String message;
+
+    private UserItemModel result;
+
+    UserApplicationModel(Optional<StorageUserItem> model, Function<StorageUserItem, UserItemModel> mapper, String message) {
+        this.model = model;
+        this.mapper = mapper;
+        this.message = message;
+        this.result = null;
+    }
+
+    public static UserApplicationModelBuilder builder() {
+        return new UserApplicationModelBuilder();
+    }
 
     @Override
     public UserItemModel getData() {
-        return mapper.apply(model.get());
+        if (result == null) {
+            result = mapper.apply(model.get());
+            return result;
+        }
+        return result;
     }
 
     @Override
@@ -29,5 +44,37 @@ public class UserApplicationModel implements ApplicationModel<UserItemModel, Use
     @Override
     public boolean hasError() {
         return model.isEmpty();
+    }
+
+    public static class UserApplicationModelBuilder {
+        private Optional<StorageUserItem> model;
+        private Function<StorageUserItem, UserItemModel> mapper;
+        private String message;
+
+        UserApplicationModelBuilder() {
+        }
+
+        public UserApplicationModelBuilder model(Optional<StorageUserItem> model) {
+            this.model = model;
+            return this;
+        }
+
+        public UserApplicationModelBuilder mapper(Function<StorageUserItem, UserItemModel> mapper) {
+            this.mapper = mapper;
+            return this;
+        }
+
+        public UserApplicationModelBuilder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public UserApplicationModel build() {
+            return new UserApplicationModel(model, mapper, message);
+        }
+
+        public String toString() {
+            return "UserApplicationModel.UserApplicationModelBuilder(model=" + this.model + ", mapper=" + this.mapper + ", message=" + this.message + ")";
+        }
     }
 }
