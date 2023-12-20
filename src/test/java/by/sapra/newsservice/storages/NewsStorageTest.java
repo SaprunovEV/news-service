3c6663c7f4d2a2d9409952ba04ebcce7bf66691f
Евгений Сапрунов <sapra10062018@gmail.com>
2023-12-20 18:13:24 +0300

    Доработать API для новостей. #55
    Подготовлен тест для фильтрации по категории.

--- src/test/java/by/sapra/newsservice/storages/NewsStorageTest.java
+++ src/test/java/by/sapra/newsservice/storages/NewsStorageTest.java
@@ -1,6 +1,8 @@
 package by.sapra.newsservice.storages;
 
 import by.sapra.newsservice.config.AbstractDataTest;
+import by.sapra.newsservice.models.Category2News;
+import by.sapra.newsservice.models.CategoryEntity;
 import by.sapra.newsservice.models.NewsEntity;
 import by.sapra.newsservice.models.UserEntity;
 import by.sapra.newsservice.services.models.filters.NewsFilter;
@@ -19,6 +21,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static by.sapra.newsservice.testUtils.Category2NewsTestDataBuilder.aCategory2News;
+import static by.sapra.newsservice.testUtils.CategoryTestDataBuilder.aCategory;
 import static by.sapra.newsservice.testUtils.NewsTestDataBuilder.aNews;
 import static by.sapra.newsservice.testUtils.UserTestDataBuilder.aUser;
 import static java.util.function.Function.identity;
@@ -75,11 +79,69 @@ class NewsStorageTest extends AbstractDataTest {
 
     }
 
+        @Test
+    void shouldFilterByCategoryId() throws Exception {
+        int pageNumber = 0;
+        int pageSize = 3;
+
+        NewsFilter filter = getNewsFilter(pageNumber, pageSize);
+
+        filter.setCategory(1L);
+
+
+        TestDataBuilder<CategoryEntity> targetCategoryBuilder = getTestDbFacade().persistedOnce(aCategory().withName("targetName"));
+        TestDataBuilder<CategoryEntity> categoryBuilder = getTestDbFacade().persistedOnce(aCategory().withName("notTargetName"));
+
+        TestDataBuilder<UserEntity> userBuilder = getTestDbFacade().persistedOnce(aUser());
+
+        List<Category2News> links = List.of(
+                saveCategoryLinks(targetCategoryBuilder, userBuilder, "title 1"),
+                saveCategoryLinks(categoryBuilder, userBuilder, "title 2"),
+                saveCategoryLinks(targetCategoryBuilder, userBuilder, "title 3"),
+                saveCategoryLinks(categoryBuilder, userBuilder, "title 4"),
+                saveCategoryLinks(targetCategoryBuilder, userBuilder, "title 5")
+        );
+
+        List<NewsEntity> expected = links.stream()
+                .filter(link -> link.getCategory().getId().equals(targetCategoryBuilder.build().getId())).map(Category2News::getNews).toList();
+
+        NewsListModel actual = newsStorage.findAll(filter);
+
+        assertAll(() -> {
+            assertNotNull(actual, "не должен возвращать null");
+            assertNotNull(actual.getNews(), "список новостей тоже не null");
+            assertEquals(pageSize, actual.getCount(), "количество новостей равно pageSize");
+            assertEquals(expected.size(), actual.getNews().size(), "ожидаемое количество равно пришедшему");
+            for (int i = 0; i < expected.size(); i++) {
+                assertModelWithCategory(expected.get(i), actual.getNews().get(i), filter.getCategory());
+            }
+        });
+    }
+
+    private void assertModelWithCategory(NewsEntity entity, NewsModel newsModel, Long category) {
+        assertAll(() -> {
+            assertModel(entity, newsModel);
+            assertTrue(newsModel.getCategoryIds().contains(category), "категория должна быть отфильтрована");
+        });
+    }
+
+    private Category2News saveCategoryLinks(TestDataBuilder<CategoryEntity> targetCategoryBuilder, TestDataBuilder<UserEntity> userBuilder, String title) {
+        return getTestDbFacade().save(
+                aCategory2News()
+                        .withCategory(targetCategoryBuilder)
+                        .withNews(getTestDbFacade().persistedOnce(
+                                aNews()
+                                        .withUser(userBuilder)
+                                        .withTitle(title)
+                        ))
+        );
+    }
+
     private void assertModel(NewsEntity expected, NewsModel actual) {
         assertAll(() -> {
-            assertEquals(expected.getId(), actual.getId());
-            assertEquals(expected.getUser().getId(), actual.getOwner());
-            assertEquals(expected.getTitle(), actual.getTitle());
+            assertEquals(expected.getId(), actual.getId(), "id должны совпадать");
+            assertEquals(expected.getUser().getId(), actual.getOwner(), "ID владельца должны совпадать");
+            assertEquals(expected.getTitle(), actual.getTitle(), "заголовки должны совпадать");
         });
     }
 
